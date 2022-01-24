ae9e511b9e64b65c94c3171eb3e72e5decf2e5bc
Raja Nandan Hrishikesh Reddy Kasula <97771733+hrishi-30@users.noreply.github.com>
2022-01-23 18:14:26 -0700

    Update Client.java

--- ChatSystem_P2P/src/client/Client.java
+++ ChatSystem_P2P/src/client/Client.java
@@ -7,8 +7,72 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
+import model.NodeInfo;
+
 
 public class Client {
+    
+    private String name;
+    NodeInfo nodeInfo;
+
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+
+    Socket socket;
+
+    public Client() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        name = sc.nextLine();
+
+        System.out.println("Write commands..");
+        while (true) {
+
+            try {
+
+                String text = sc.nextLine();
+                String[] parts = text.split(" ");
+
+                if (parts[0].equals("JOIN")) {
+                    //Read from file
+                    nodeInfo = new NodeInfo(parts[1], Integer.parseInt(parts[2]), name);
+                    socket = new Socket(nodeInfo.getIP(), nodeInfo.getPort());
+                    oos = new ObjectOutputStream(socket.getOutputStream());
+                    ois = new ObjectInputStream(socket.getInputStream());
+
+                    Message msg = new Message("JOIN", nodeInfo);
+                    oos.writeObject(msg);
+                    System.out.println("Chat Group Joined!");
+
+                    Thread thread = new Thread(() -> {
+                        while (true) {
+                            try {
+                                if (socket != null) {
+                                    Message msg1 = (Message) ois.readObject();
+                                    if (msg1.getType().equals("NOTE")) {
+                                        System.out.println(msg1.getContent());
+                                    } else if (msg1.getType().equals("SHUTDOWN ALL")) {
+                                        System.out.println("Server is closed!");
+                                        socket.close();
+                                        System.exit(0);
+                                    }
+                                }
+                            } catch (IOException | ClassNotFoundException e) {
+                                //e.printStackTrace();
+                            }
+                        }
+                    });
+                    thread.start();
+                }
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
 
     /**
      * @param args the command line arguments
