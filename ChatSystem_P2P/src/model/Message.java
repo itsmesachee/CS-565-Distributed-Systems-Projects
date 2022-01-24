c8d88325fb47a1290890b1b7e9a7d40c2c71f793
Raja Nandan Hrishikesh Reddy Kasula <97771733+hrishi-30@users.noreply.github.com>
2022-01-23 18:18:52 -0700

    Update Message.java

--- ChatSystem_P2P/src/model/Message.java
+++ ChatSystem_P2P/src/model/Message.java
@@ -1,1 +1,36 @@
-#message interface class
+package model;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+    private String type;
+    private Object content;
+
+    public Message(String type, Object content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content.toString();
+    }
+
+}
