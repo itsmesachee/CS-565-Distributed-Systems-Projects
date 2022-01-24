dc0a49c618eb1abb1c1c8bd2998dd6f906ee80be
SaChee <29009536+daystalker@users.noreply.github.com>
2022-01-23 18:19:55 -0700

    Update NodeInfo.java

--- ChatSystem_P2P/src/model/NodeInfo.java
+++ ChatSystem_P2P/src/model/NodeInfo.java
@@ -1,1 +1,33 @@
-#Node information interface class
+package model;
+
+import java.io.Serializable;
+
+public class NodeInfo implements Serializable{
+
+    private String IP;
+    private int port;
+    private String name;
+
+    public NodeInfo(String IP, int port, String name) {
+        this.IP = IP;
+        this.port = port;
+        this.name = name;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
