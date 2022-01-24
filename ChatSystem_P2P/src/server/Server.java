7f5e4d67b5b76b9c345b84857e9cf06eebee8d1a
Sai <29009536+daystalker@users.noreply.github.com>
2022-01-23 23:37:53 -0700

    Update Server.java

--- ChatSystem_P2P/src/server/Server.java
+++ ChatSystem_P2P/src/server/Server.java
@@ -17,6 +17,9 @@ public class Server implements MessageTypes {
     private ServerSocket ss;
 
     public Server() throws IOException {
+        
+    	//for reducing ambiguity, i have hardcardoded the port number to avoid any unexpected closed ports.
+        
         ss = new ServerSocket(1012);
         System.out.println("Waiting for client...");
 
@@ -24,6 +27,12 @@ public class Server implements MessageTypes {
             Socket s = null;
 
             try {
+                
+            	/*
+            	 * Opening the socket connection and creating streams laying paths for clients to communicate with server
+            	 * and between themselves.
+            	 */
+                
                 s = ss.accept();
 
                 ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
@@ -41,11 +50,19 @@ public class Server implements MessageTypes {
 
     public static void main(String[] args) throws IOException {
         Server server = new Server();
+        
+        
+        // Creating a server object through main() which initiates the next steps in commencement of server.
+ 
     }
 
 // ClientHandler class
     class ClientHandler extends Thread {
 
+    	/*
+    	 * This class is specifically used to handle multiple clients and facilitate communication betwwen them.
+    	 */
+        
         final ObjectInputStream ois;
         final ObjectOutputStream oos;
         final Socket s;
@@ -66,6 +83,13 @@ public class Server implements MessageTypes {
                 while (true) {
 
                     Message message = (Message) ois.readObject();
+                    
+                    /*
+                     * Based on the type of message constant which matches to the command sent from a client to the server, respective
+                     * code block goes lives on matching the condition
+                     */
+
+
 
                     if (message.getType() == MessageTypes.JOIN) {
                         // If new user sends JOIN message, add user to the Arraylist
@@ -73,7 +97,11 @@ public class Server implements MessageTypes {
                         NodeInfo info = (NodeInfo) message.getContent();
                         System.out.println(info.getName() + " has joined!");
                     } else if (message.getType() == MessageTypes.LEAVE) {
-                        // If user sends LEAVE message, remove user from Arraylist
+                        /* If user sends LEAVE message, remove user from Arraylist
+                         *
+                    	 * closing the streams and giving a prompt/debugging info to the client and on server too.
+                    	 */
+                        
                         outStreams.remove(oos);
                         ois.close();
                         oos.close();
@@ -82,6 +110,11 @@ public class Server implements MessageTypes {
                         break;
                     } else if (message.getType() == MessageTypes.NOTE) {
                         // If user sends a regular string, send the message string to all users
+                        /*
+                    	 * Server side implementation of passing/forwading the messages/communication happening between clients
+                    	 * facilitated by the chat connection through server.
+                    	 */
+                        
                         System.out.println(message.getContent());
                         if (outStreams.contains(oos)) {
                             for (ObjectOutputStream stream : outStreams) {
@@ -90,6 +123,12 @@ public class Server implements MessageTypes {
                         }
                     } else if (message.getType() == MessageTypes.SHUTDOWN_ALL) {
                         // If user sends SHUTDOWN ALL, terminate all connections and shut down the server
+                        
+                        
+                    	/*
+                    	 * posting a message to all chat-connected clients and on serves as well.
+                    	 */
+                        
                         System.out.println("Server is shutting down...");
                         for (ObjectOutputStream stream : outStreams) {
                             stream.writeObject(message);
