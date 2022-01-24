53b3d0359b4f63400e384041489ccaf78a751599
SaChee <29009536+daystalker@users.noreply.github.com>
2022-01-23 18:11:45 -0700

    Update Client.java

--- ChatSystem_P2P/src/client/Client.java
+++ ChatSystem_P2P/src/client/Client.java
@@ -1,1 +1,33 @@
-client
+package client;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import model.Message;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
+
+    private NodeInfo readConnectionInfo() {
+        try {
+            Scanner scanner = new Scanner(new FileInputStream("properties.txt"));
+            String[] parts = scanner.nextLine().split(" ");
+            String ip = parts[0];
+            int port = Integer.parseInt(parts[1]);
+            return new NodeInfo(ip, port, name);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        return null;
+    }
+
+}
