24bf3ee27f53eb57b871118af5289a59eca3297f
Sai <29009536+daystalker@users.noreply.github.com>
2022-02-27 15:54:31 -0700

    Update NodeInfo.java

--- Distributed Chat/Ring/CS-565---Distributed-Systems---Projects-main/ChatSystem_P2P/src/model/NodeInfo.java
+++ Distributed Chat/Ring/CS-565---Distributed-Systems---Projects-main/ChatSystem_P2P/src/model/NodeInfo.java
@@ -12,7 +12,7 @@ public class NodeInfo implements Serializable
     ObjectOutputStream objectOutputStream;
 
     NodeInfo nextNode;
-
+    // This method holds the current node information
     public NodeInfo(String IP, int port, String name)
     {
         this.IP = IP;
@@ -20,42 +20,41 @@ public class NodeInfo implements Serializable
         this.name = name;
     }
 
-
+    // This method is used to get the IP Address
     public String getIP()
     {
         return IP;
     }
-
+    // This method is used to get the Port number
     public int getPort()
     {
         return port;
     }
-
+    // This method is used to get the data in the next node
     public NodeInfo getNextNode()
     {
         return nextNode;
     }
-
+    // This method is used to set the data in the next node 
     public void setNextNode(NodeInfo nextNode)
     {
         this.nextNode = nextNode;
     }
-
+     // This method is used to set the IP Address while joining the chat
     public void setIP(String IP)
     {
         this.IP = IP;
     }
-
+    // This method is used to set the port number while joining the chat
     public void setPort(int port)
     {
         this.port = port;
     }
-
+    // This method is used to get the name of next node
     public String getName()
     {
         return name;
     }
-
     public ObjectOutputStream getObjectOutputStream()
     {
         return objectOutputStream;
