6f4435fbbffee072bdaf8b040133d45246b94d2b
ShriyaSridharNAU <91515608+ShriyaSridharNAU@users.noreply.github.com>
2022-03-01 05:51:47 -0700

    Update NodeInfo.java

--- Distributed Chat/Ring/CS-565---Distributed-Systems---Projects-main/ChatSystem_P2P/src/model/NodeInfo.java
+++ Distributed Chat/Ring/CS-565---Distributed-Systems---Projects-main/ChatSystem_P2P/src/model/NodeInfo.java
@@ -6,13 +6,18 @@ import java.io.Serializable;
 public class NodeInfo implements Serializable
 {
 
+    /*
+     *  This class stores information about the current client such as the IP address, port, name,
+     *  and left and right nodes. Getters and setters are used to access this data from other classes.
+     * */
+
     private String IP;
     private int port;
     private String name;
-    ObjectOutputStream objectOutputStream;
+    transient ObjectOutputStream objectOutputStream;
 
     NodeInfo nextNode;
-    // This method holds the current node information
+
     public NodeInfo(String IP, int port, String name)
     {
         this.IP = IP;
@@ -20,41 +25,42 @@ public class NodeInfo implements Serializable
         this.name = name;
     }
 
-    // This method is used to get the IP Address
+
     public String getIP()
     {
         return IP;
     }
-    // This method is used to get the Port number
+
     public int getPort()
     {
         return port;
     }
-    // This method is used to get the data in the next node
+
     public NodeInfo getNextNode()
     {
         return nextNode;
     }
-    // This method is used to set the data in the next node 
+
     public void setNextNode(NodeInfo nextNode)
     {
         this.nextNode = nextNode;
     }
-     // This method is used to set the IP Address while joining the chat
+
     public void setIP(String IP)
     {
         this.IP = IP;
     }
-    // This method is used to set the port number while joining the chat
+
     public void setPort(int port)
     {
         this.port = port;
     }
-    // This method is used to get the name of next node
+
     public String getName()
     {
         return name;
     }
+
     public ObjectOutputStream getObjectOutputStream()
     {
         return objectOutputStream;
